2b05992acc351dc5f38703f35057facebe3b791a
Marccel Janara <nengahyuichiro34@gmail.com>
2024-09-23 12:19:24 +0700

    port

--- app.js
+++ app.js
@@ -29,4 +29,4 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen(5000, () => console.log('Server running at port 5000'));
+app.listen(process.env.PORT, () => console.log(`Server running at port ${process.env.PORT}`));
